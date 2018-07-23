0dd16a328184fa45ce8eb5bb2dcc9dcba2c417f1
oysstu <oysstu@gmail.com>
2018-07-23 10:37:23 +0200

    Release version 1.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def main():
 
     # Run setup script
     setup(name='pyxtf',
-          version='1.0',
+          version='1.1',
           description='eXtended Triton Format (XTF) file interface',
           long_description=long_description,
           long_description_content_type='text/markdown',
